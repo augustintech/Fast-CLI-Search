afad1efe35709afaaf63478ad30ce7730ce5a147
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-05-11 18:49:31 +0100

    Possibility to exit script. Small formatting.

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 # Version: Python3
 
-findme = input("Enter a name to get their DOB")
-print("Currently searching for ",findme)  
-
+findme = input("Enter a name to get their DOB: ")
+print("Currently searching for",findme)  
+quit()
