f8a830e818b833da03ea1d2dc637b3b510c8893a
greenlight <44822931+greenLight8@users.noreply.github.com>
2019-05-12 12:14:53 +0100

    Check if page exists

--- search.py
+++ search.py
@@ -3,6 +3,26 @@ import requests
 # Version: Python3
 
 findme = input("Enter a name to get their DOB: ")
-print("Currently searching for",findme)  
-quit()
+print("Currently searching for",findme)
 
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+SEARCHPAGE = findme
+
+PARAMS = {
+    'action':"query",
+    'list':"search",
+    'srsearch': SEARCHPAGE,
+    'format':"json",
+    
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+if DATA['query']['search'][0]['title'] == SEARCHPAGE:
+    print("Your search page '" + SEARCHPAGE + "' exists on English Wikipedia")
